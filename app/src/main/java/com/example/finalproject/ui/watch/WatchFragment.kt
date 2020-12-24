98307c4e2a40430656c92d992d4568fd54d1ea0d
Gardendripar <1318247793@qq.com>
2020-12-24 20:39:33 +0800

    第一个App整合

--- app/src/main/java/com/example/finalproject/ui/watch/WatchFragment.kt
+++ app/src/main/java/com/example/finalproject/ui/watch/WatchFragment.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 
 import com.example.finalproject.R
+import kotlinx.android.synthetic.main.fragment_watch.*
 
 class WatchFragment : Fragment() {
 
@@ -27,7 +29,24 @@ class WatchFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(WatchViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel.seconds.observe(this, Observer {
+            val hours = it / 3600
+            val minutes = (it % 3600) / 60
+            val secs = it % 60
+            textView_time.text = String.format("%02d:%02d:%02d", hours, minutes, secs)
+        })
+
+        button_start.setOnClickListener {
+            viewModel.start()
+        }
+
+        button_start.setOnClickListener {
+            viewModel.stop()
+        }
+
+        button_start.setOnClickListener {
+            viewModel.restart()
+        }
     }
 
 }

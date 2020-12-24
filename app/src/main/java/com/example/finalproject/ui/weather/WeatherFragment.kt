0cbbd15e8942fbbb9427d07d6cc7e3929e5e4385
Gradendripar <1318247793@qq.com>
2020-12-24 18:51:01 +0800

    第一次提交

---
+++ app/src/main/java/com/example/finalproject/ui/weather/WeatherFragment.kt
@@ -0,0 +1,33 @@
+package com.example.finalproject.ui.weather
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.finalproject.R
+
+class WeatherFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = WeatherFragment()
+    }
+
+    private lateinit var viewModel: WeatherViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_weather, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(WeatherViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
